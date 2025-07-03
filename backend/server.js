50689840a473fb132b3d52c64236a143999efa26
J-Shotayo <damolatunde17@gmail.com>
2025-07-03 22:48:12 +0100

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,24 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+const cors = require('cors');
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://task-manager-frontend-two-psi.vercel.app/'
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 // Routes
 app.use('/api/tasks', tasksRouter);
 
